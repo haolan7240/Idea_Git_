4bdc62d32e6126ed6a8481997318b3331c1831c7
Xu <2976383964@qq.com>
2022-10-28 16:02:59 +0800

    Git_Second_Commit

--- src/main/java/com/haolan/git/Haolan_Git.java
+++ src/main/java/com/haolan/git/Haolan_Git.java
@@ -2,6 +2,8 @@ package com.haolan.git;
 
 public class Haolan_Git {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git\n");
+        System.out.println("haha");
+
     }
 }
